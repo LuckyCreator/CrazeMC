6c6cfa11d06e7aecc2064f0ec8c5b21c81fc7e63
LuckyCreator <44301423+LuckyCreator@users.noreply.github.com>
2021-09-21 11:14:29 +0200

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -1,1 +1,25 @@
+const discord = require("discord.js");
 
+module.exports.run = async(bot, message, args) => {
+    
+    var logChannel = message.guild.channels.cache.find(channel => channel.name === "//ENTER LOG NAME//");
+
+    if(message.guild.me.hasPermission(`MANAGE_CHANNELS`)) {
+        message.channel.delete();
+        let Embed = new discord.MessageEmbed()
+            .setTitle(`Ticket Closed!`)
+            .setDescription(`**Ticket Name:** ${message.channel.name}\n**Gesloten door:** <@${message.author.id}>`)
+            .setColor(`#ab0a7d`)
+            .setFooter("CrazeMC | MC-Server", "https://i.imgur.com/LvaXWkZ.jpg")
+        logChannel.send(Embed);
+    }
+
+    if(!message.guild.me.hasPermission(`MANAGE_CHANNELS`)) {
+        message.guild.send("Sorry maar je hebt hier helaas geen rechten voor.")
+    }
+
+}
+
+module.exports.help = {
+    name: "close"
+}
